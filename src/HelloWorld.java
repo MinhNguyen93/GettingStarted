3a235e34a1f3cdc01e9ae946266a0f98b1fca146
minh <minhnt1993@gmail.com>
2018-07-27 15:08:09 +0700

    This is my second commit, just a simple comment.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
+    //This is test comment, I'm practicing with git a Github
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
 }
